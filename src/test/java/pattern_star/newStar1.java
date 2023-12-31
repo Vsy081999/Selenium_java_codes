53952a9bb138d4844025dc09eaab696735f971ea
Vishwanath Yadav <Vsy081999@gmail.com>
2023-12-31 22:36:16 +0530

    5 commit

--- src/test/java/pattern_star/newStar1.java
+++ src/test/java/pattern_star/newStar1.java
@@ -1,14 +1,10 @@
 package pattern_star;
 
-public class new_star {
+public class newStar1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("test");
-
-		System.out.println("test");
-		
 	}
 
 }
